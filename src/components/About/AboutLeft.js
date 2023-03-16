31e1b85a049bf20b70497537b16e23c9f26c3306
Emmanuel Taiwo <devemmanuel1@gmail.com>
2023-03-16 09:06:57 +0100

    Fixed vercel deployment error

--- src/components/About/AboutLeft.js
+++ src/components/About/AboutLeft.js
@@ -13,10 +13,10 @@ const AboutLeft = () => {
         I like making music, I also like playing video game, But I also like sharing content related to the stuff that I have learned over the years in Web Development I do this to help other people in the Dev Community. 
         <br /><br />
         
-        Feel free to Connect or Follow me on my <a href="https://www.linkedin.com/in/realemmanuel/" className='text-blue-800'>Linkedin</a> where I post useful content related to Web Development and Programming; here's my Twitter, in case you like memes 😜.
+        Feel free to Connect or Follow me on my <a href="https://www.linkedin.com/in/realemmanuel/" className='text-blue-800'>Linkedin</a> where I post useful content related to Web Development and Programming; here&apos;s my Twitter, in case you like memes 😜.
         <br /><br />
 
-        I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience, please don&apos;t hesitate to contact me.</p>
+        I&apos;m open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience, please don&apos;t hesitate to contact me.</p>
 
         <div onClick={() => router.push('/contact')}>
         <Button>Contact me</Button>
