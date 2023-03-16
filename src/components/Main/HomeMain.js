89637cf4e8c06074822920537eaad913ed03ca14
Emmanuel Taiwo <devemmanuel1@gmail.com>
2023-03-16 09:18:41 +0100

    Reduced the Home page Text for mobile view

--- src/components/Main/HomeMain.js
+++ src/components/Main/HomeMain.js
@@ -9,7 +9,7 @@ const HomeMain = () => {
         backgroundImage: `url("https://i.ibb.co/nD6yJMB/hero.png")` 
       }}>
 
-      <h1 className='text-center ml-auto mr-auto px-3 text-5xl sm:text-left font-black font-righteous'>HEY, I&apos;M EMMANUEL TAIWO</h1>
+      <h1 className='text-center ml-auto mr-auto px-3 text-3xl xl:text-6xl md:text-5xl sm:text-left font-black font-righteous'>HEY, I&apos;M EMMANUEL TAIWO</h1>
       <p className='text-center ml-auto mr-auto px-3 text-gray-600'>A Frontend developer building my version of the digital world one step at a time.</p>
       <div onClick={() => router.push('/projects')} className='ml-auto mr-auto'>
       <Button>Projects</Button>
