856659f73bfda78214d8378c2673ea1bdc534c09
Emmanuel Taiwo <devemmanuel1@gmail.com>
2023-03-16 09:10:12 +0100

    Fixed useEffect Error

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,10 @@
 import Header from '@/components/Header/Header'
-import React, { useEffect } from 'react'
+import React from 'react'
 import Head from 'next/head'
 import AboutMain from '@/components/About/AboutMain'
 import Footer from '@/components/Footer/Footer'
 
 const about = () => {
-  useEffect(() => {
-    const handleContextmenu = e => {
-        e.preventDefault()
-    }
-    document.addEventListener('contextmenu', handleContextmenu)
-    return function cleanup() {
-        document.removeEventListener('contextmenu', handleContextmenu)
-    }
-}, [ ])
   return (
     <>
     <Head>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,19 +1,10 @@
 import Header from '@/components/Header/Header'
-import React, { useEffect } from 'react'
+import React from 'react'
 import Head from 'next/head'
 import ProjectMain from '@/components/Projects/ProjectMain'
 import Footer from '@/components/Footer/Footer'
 
 const projects = () => {
-  useEffect(() => {
-    const handleContextmenu = e => {
-        e.preventDefault()
-    }
-    document.addEventListener('contextmenu', handleContextmenu)
-    return function cleanup() {
-        document.removeEventListener('contextmenu', handleContextmenu)
-    }
-}, [ ])
   return (
     <>
     <Head>
