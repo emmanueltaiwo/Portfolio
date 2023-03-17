9a062a1e98719f14eac598a154e96c09e6a4382f
Emmanuel Taiwo <devemmanuel1@gmail.com>
2023-03-17 08:38:26 +0100

    Added Waving Emoji in Welcome Screen and Made Header Logo a Link

--- src/components/Main/HomeMain.js
+++ src/components/Main/HomeMain.js
@@ -7,7 +7,7 @@ const HomeMain = () => {
     const router = useRouter()
   return (
     <HomeBackground>
-        <h1 className='text-center ml-auto mr-auto px-3 text-4xl xl:text-6xl md:text-5xl sm:text-left font-black font-righteous'>HEY, I&apos;M EMMANUEL TAIWO</h1>
+        <h1 className='text-center ml-auto mr-auto px-3 text-4xl xl:text-6xl md:text-5xl sm:text-left font-black font-righteous'>HEY, I&apos;M EMMANUEL TAIWO 👋</h1>
         <p className='text-center ml-auto mr-auto px-3 text-gray-600'>A Frontend developer building my version of the digital world one step at a time.</p>
         <div onClick={() => router.push('/projects')} className='ml-auto mr-auto'>
           <Button>Projects</Button>
