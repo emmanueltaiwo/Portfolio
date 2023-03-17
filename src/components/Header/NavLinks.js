b1cdfb677a4a0a615eef22b1326aadbde67fe81d
Emmanuel Taiwo <devemmanuel1@gmail.com>
2023-03-17 08:35:08 +0100

    Made Logo a link

--- src/components/Header/NavLinks.js
+++ src/components/Header/NavLinks.js
@@ -11,8 +11,10 @@ const NavLinks = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
-     <span className='tracking-widest cursor-pointer select-none'>👦 EMMANUEL TAIWO</span>
-    </div>
+              <span onClick={() => router.push('/')} className='tracking-widest cursor-pointer select-none'>
+                👦 EMMANUEL TAIWO
+              </span>
+          </div>
             <div className="ml-auto flex items-center">
               <div className="ml-auto hidden md:block">
                 <div className="ml-auto flex items-end space-x-10">
