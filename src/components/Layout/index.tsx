c7a4b45199d9b26c1c01594540788497f1812a06
Emmanuel TAIWO <emmanueltaiwo@Emmanuels-MacBook-Pro.local>
2023-11-11 19:12:24 +0100

    fix:fixed type import error

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment, ReactNode } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-
 interface LayoutProps {
   children: ReactNode;
   userHasScroll: boolean;
